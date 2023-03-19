2bf99cd66c0631e9beb0ac41cfcd236c23fdaa6a
chard2023 <richard.vistal012@gmail.com>
2023-03-19 11:28:07 +0800

    update apiurl environment

--- src/env.js
+++ src/env.js
@@ -1,3 +1,7 @@
+const isSecure = window.location.protocol === "https:";
+
+const API_BASE_URL = isSecure ? "https://driving-school-backend.vercel.app/api/" : "http://localhost:8080/api/";
+
 module.exports = {
-  API_BASE_URL: 'http://localhost:8080/api/',
+  API_BASE_URL: API_BASE_URL,
 };
